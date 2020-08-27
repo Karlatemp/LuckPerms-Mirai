9d815eb65bcb863f77e0bdea341a4bb75282bbbd
Karlatemp <karlatemp@vip.qq.com>
2020-08-28 02:13:16 +0800

    Create license

--- src/main/kotlin/io/github/karlatemp/luckperms/mirai/WrappedCommandSender.kt
+++ src/main/kotlin/io/github/karlatemp/luckperms/mirai/WrappedCommandSender.kt
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ *
+ * LuckPerms-Mirai/LuckPerms-Mirai.main/WrappedCommandSender.kt
+ *
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found via the following link.
+ *
+ * https://github.com/Karlatemp/LuckPerms-Mirai/blob/master/LICENSE
+ */
+
 package io.github.karlatemp.luckperms.mirai
 
 import net.mamoe.mirai.Bot
--- src/main/kotlin/io/github/karlatemp/luckperms/mirai/commands/WrappedLPSender.kt
+++ src/main/kotlin/io/github/karlatemp/luckperms/mirai/commands/WrappedLPSender.kt
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ *
+ * LuckPerms-Mirai/LuckPerms-Mirai.main/WrappedLPSender.kt
+ *
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found via the following link.
+ *
+ * https://github.com/Karlatemp/LuckPerms-Mirai/blob/master/LICENSE
+ */
+
 package io.github.karlatemp.luckperms.mirai.commands
 
 import me.lucko.luckperms.common.command.access.CommandPermission
